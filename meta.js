7f348a341dc7d334f0e8f6a363173badfcb9a754
unheal1337 <tobiaszylka@gmail.com>
2019-10-14 11:41:03 +0200

    Update meta.js

--- meta.js
+++ meta.js
@@ -5,4 +5,13 @@ tronWeb = window.tronWeb;
 tycoon = await tronWeb.contract().at("TPexUXMe63YHcARbMnhDRdcmsk3SWJ1fWN");
 currentAddr = tronWeb.defaultAddress['base58'];
 
-console.log(currentAddr);
+async function main() {
+
+        tronWeb = window.tronWeb;
+        wax = await tronWeb.contract().at("TPexUXMe63YHcARbMnhDRdcmsk3SWJ1fWN");
+         
+        currentAddr = tronWeb.defaultAddress['base58'];
+  console.log(currentAddr);
+}
+
+main();
