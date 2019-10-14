73cfe1f06355f798ff22821a3ca1b4c078ddc848
unheal1337 <tobiaszylka@gmail.com>
2019-10-14 12:09:38 +0200

    Update meta.js

--- meta.js
+++ meta.js
@@ -8,7 +8,17 @@ async function main() {
         wax = await tronWeb.contract().at("TPexUXMe63YHcARbMnhDRdcmsk3SWJ1fWN");
          
         currentAddr = tronWeb.defaultAddress['base58'];
-  console.log(currentAddr);
+        console.log(currentAddr);
 }
+
+function getRvFromUnit(unit) {
+        tycoon.myrv(unit).call().then(result => {
+                console.log("myrv", result);
+                var data = (result.toString())/2e6;
+                var myrv = data.toFixed(3);
+                //document.getElementById("rv" ).textContent = myrv;
+        });
+}
+
 
 main();
