0fa6f3cf3fb53b08e8d3909825616ceaadefeac4
unheal1337 <tobiaszylka@gmail.com>
2019-10-14 11:43:18 +0200

    Update meta.js

--- meta.js
+++ meta.js
@@ -1,9 +1,6 @@
 var tronWeb;
 var currentAddr;
 
-tronWeb = window.tronWeb;
-tycoon = await tronWeb.contract().at("TPexUXMe63YHcARbMnhDRdcmsk3SWJ1fWN");
-currentAddr = tronWeb.defaultAddress['base58'];
 
 async function main() {
 
