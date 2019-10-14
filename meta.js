8756d2a6e642d5cbdb01f494e200e19dc17b1681
unheal1337 <tobiaszylka@gmail.com>
2019-10-14 12:18:01 +0200

    Update meta.js

--- meta.js
+++ meta.js
@@ -1,15 +1,28 @@
 var tronWeb;
 var currentAddr;
+var waiting = 0;
 
 
 async function main() {
+        
+        if (typeof(window.tronWeb) === 'undefined') {
+                console.log('Waiting for tronWeb...');
+                waiting += 1;
+          
+                setTimeout(main, 1000);
+        } else {
+                tronWeb = window.tronWeb;
+                tycoon = await tronWeb.contract().at("TPexUXMe63YHcARbMnhDRdcmsk3SWJ1fWN");
 
-        tronWeb = window.tronWeb;
-        tycoon = await tronWeb.contract().at("TPexUXMe63YHcARbMnhDRdcmsk3SWJ1fWN");
-         
-        currentAddr = tronWeb.defaultAddress['base58'];
-        console.log(currentAddr);
-        getRvFromUnit(1);
+                currentAddr = tronWeb.defaultAddress['base58'];
+                console.log(currentAddr);
+                getRvFromUnit(1);
+                
+                /*setTimeout(function() {}, 2000);
+                setInterval(function() {
+                        mainloop();
+                }, 2000);*/
+        }
 }
 
 function getRvFromUnit(unit) {
